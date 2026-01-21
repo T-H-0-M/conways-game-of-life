ce3602b4c6007e6e0a53b6826ffa2e913d98659e
T_H_0_M <thomasbandy23@gmail.com>
2026-01-21 21:52:03 +1100

    feat: concurrent cell calculations!

--- src/main/java/com/thom/gameoflife/GameOfLife.java
+++ src/main/java/com/thom/gameoflife/GameOfLife.java
@@ -1,5 +1,12 @@
 package com.thom.gameoflife;
 
+import java.lang.Runnable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+// public class GameOfLife implements Runnable {
 public class GameOfLife {
     public final byte ALIVE = 1;
     public final byte DEAD = 0;
@@ -8,12 +15,19 @@ public class GameOfLife {
     private int columns;
     private byte[][] currentBoard; // readboard
     private byte[][] nextBoard;
+    private ExecutorService gameEngine;
+    private CyclicBarrier barrier;
 
     public GameOfLife(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         this.currentBoard = new byte[rows][columns];
         this.nextBoard = new byte[rows][columns];
+        // TODO: make this dynamic based on board size?
+        this.gameEngine = Executors.newFixedThreadPool(10);
+        // INFO: this needs to be 11, always account for the main thread or it will eat
+        // your lunch
+        this.barrier = new CyclicBarrier(11);
     }
 
     public int countActiveNeighbors(int row, int col) {
@@ -32,22 +46,52 @@ public class GameOfLife {
         return activeNeighbors;
     }
 
+    // @Override
+    // public void run() {
+    //
+    // }
+
     public void step() {
-        for (int i = 0; i < this.rows; i++) {
-            for (int j = 0; j < this.columns; j++) {
-                int activeNeighbors = countActiveNeighbors(i, j);
-                byte currentState = currentBoard[i][j];
-                if (currentState == ALIVE && activeNeighbors < 2) {
-                    nextBoard[i][j] = DEAD;
-                } else if (currentState == ALIVE && activeNeighbors > 3) {
-                    nextBoard[i][j] = DEAD;
-                } else if (currentState == DEAD && activeNeighbors == 3) {
-                    nextBoard[i][j] = ALIVE;
-                } else {
-                    nextBoard[i][j] = currentState;
+        // TODO: make this block instead of col
+        // INFO: start col is inclusive
+        for (int i = 0; i < 10; ++i) {
+            final int startCol = i * 100;
+            final int endCol = startCol + 100;
+            final int threadNum = i;
+
+            // just my personal memo - execute returns void, submit returns Future<T>
+            gameEngine.execute(() -> {
+                try {
+                    System.out.println("thread " + threadNum);
+                    for (int x = startCol; x < endCol; x++) {
+                        for (int y = 0; y < this.columns; y++) {
+                            int activeNeighbors = countActiveNeighbors(x, y);
+                            byte currentState = currentBoard[x][y];
+                            if (currentState == ALIVE && activeNeighbors < 2) {
+                                nextBoard[x][y] = DEAD;
+                            } else if (currentState == ALIVE && activeNeighbors > 3) {
+                                nextBoard[x][y] = DEAD;
+                            } else if (currentState == DEAD && activeNeighbors == 3) {
+                                nextBoard[x][y] = ALIVE;
+                            } else {
+                                nextBoard[x][y] = currentState;
+                            }
+                        }
+                    }
+                    barrier.await();
+                } catch (Exception e) {
+                    System.out.println("yuh, thread broken");
                 }
-            }
+            });
+
         }
+
+        try {
+            barrier.await();
+        } catch (Exception e) {
+            System.out.println("yuh, broken");
+        }
+
         // INFO: this is done because currentBoard = nextBoard will reference the same
         // obj after one iteration
         byte[][] tmp = currentBoard;
